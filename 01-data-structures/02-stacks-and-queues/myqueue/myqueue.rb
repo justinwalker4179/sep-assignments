b9c3e0315b368eba4201b52a2ce19e6268061cdd
justinwalker4179 <jrwalker314@yahoo.com>
2018-02-20 17:02:34 -0600

    Created stack and queue classes.

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -4,15 +4,31 @@ class MyQueue
 
   def initialize
     @queue = Array.new
-    @head = @queue[0]
+    @head = @queue.first
   end
-
+  
+  # Adds element to back of queue.
   def enqueue(element)
+    @queue << element
+    resetPointers
   end
 
+  # Removes and returns element from front of queue.
   def dequeue
+    item = @queue.delete_at(0)
+    resetPointers
+    return item
   end
 
+  # Returns true if queue is empty.
   def empty?
+    @queue.length === 0 ? true : false
+  end
+
+  private
+  # Assigns the head and tail values to first and last items of the queue respectively.
+  def resetPointers
+    self.head = @queue.first
+    self.tail = @queue.last
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -5,13 +5,22 @@ class MyStack
     @stack = Array.new
     self.top = nil
   end
-
+  # Adds item to top of stack.
   def push(item)
+    @stack << item
+    self.top != nil ? self.top+=1 : self.top=0 # Turns top from nil into 0 or increments top.
   end
-
+  # Removes and returns top of stack.
   def pop
+    topOfStack = @stack[self.top-1]
+    @stack.delete_at(self.top-1)
+
+    self.top != 0 ? self.top-=1 : self.top=nil # Returns top to nil when stack is empty or decrements top.
+    return topOfStack
   end
 
+  # Returns true if stack is empty.
   def empty?
+    @stack.length === 0 ? true : false
   end
 end
