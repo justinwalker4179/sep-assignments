c9df216047060184bfe5dc6cb446e9443ea2d320
justinwalker4179 <jrwalker314@yahoo.com>
2018-04-22 15:35:52 -0700

    Fixed seperate_chaining.rb.

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -14,46 +14,40 @@ class SeparateChaining
 
     if @items[hash_key] == nil
       link_list = LinkedList.new()
-      link_list.add_to_front(node)
+      link_list.add_to_tail(node)
       @items[hash_key] = link_list
     else
       @items[hash_key].add_to_tail(node)
-      @items[hash_key].print
     end
+
+    self.resize if self.load_factor > self.max_load_factor
   end
 
   def [](key)
     hash_key = self.index(key,self.size)
     link_list = @items[hash_key]
     current_node = link_list.head
-
-    until current_node.key == key
+    until current_node == nil || current_node.key == key
       current_node = current_node.next
     end
-
-    return current_node.value
+    if current_node == nil
+      puts "Not found"
+      return nil
+    else
+      return current_node.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
-    char_value = 0
-    # Adds together all of the ASCII values for each character into char_value.
-    for letter in key.chars do
-      char_value += letter.ord
-    end
-    # Increments char_value until its length equals desired size.
-    until char_value.to_s.length == 7
-      char_value*=11
-    end
-
-    return char_value%size
+    return key.sum%size
   end
 
   # Calculate the current load factor
   def load_factor
-    values = 0
+    values = 0.0
     for item in @items do
       if item != nil
         current_node = item.head
@@ -63,7 +57,7 @@ class SeparateChaining
         end
       end
     end
-    return values.to_f/@items.length
+    return values/@items.length
   end
 
   # Simple method to return the number of items in the hash
@@ -73,7 +67,6 @@ class SeparateChaining
 
   # Resize the hash
   def resize
-    puts "RESIZING"
     temp_array = Array.new(@items)
     @items = Array.new(temp_array.length*2)
 
@@ -109,21 +102,3 @@ class SeparateChaining
     puts "_____________________"
   end
 end
-
-hash = SeparateChaining.new(4)
-
-hash["key"] = "value"
-hash["key2"] = "value 2"
-hash["key4"] = "value 3"
-
-
-hash.display
-hash.load_factor
-
-
-#star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
-#star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
-#star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
-#star_wars_movies["Star Wars: A New Hope"] = "Number Four"
-#star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
-#star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
